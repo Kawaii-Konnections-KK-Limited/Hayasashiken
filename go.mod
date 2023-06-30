c420b236092824722067bdc6cd6c0f201e9fa96f
parsa <hparsa1378@gmail.com>
2023-06-30 16:43:27 +0330

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/uussoop/v2ray_test
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func UrlTest(client *http.Client, link string, timeout int32) (int32, error) {
+	if client == nil {
+		return 0, fmt.Errorf("no client")
+	}
+
+	// Test handshake time
+	var time_start time.Time
+	var times = 1
+	var rtt_times = 1
+
+	// Test RTT "true delay"
+	if link2 := strings.TrimLeft(link, "true"); link != link2 {
+		link = link2
+		times = 3
+		rtt_times = 2
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Millisecond)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", link, nil)
+	req.Header.Set("User-Agent", fmt.Sprintf("curl/7.%d.%d", rand.Int()%84, rand.Int()%2))
+	if err != nil {
+		return 0, err
+	}
+
+	for i := 0; i < times; i++ {
+		if i == 1 || times == 1 {
+			time_start = time.Now()
+		}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			return 0, err
+		}
+		resp.Body.Close()
+	}
+
+	return int32(time.Since(time_start).Milliseconds() / int64(rtt_times)), nil
+}
+
+func TcpPing(address string, timeout int32) (ms int32, err error) {
+	startTime := time.Now()
+	c, err := net.DialTimeout("tcp", address, time.Duration(timeout)*time.Millisecond)
+	endTime := time.Now()
+	if err == nil {
+		ms = int32(endTime.Sub(startTime).Milliseconds())
+		c.Close()
+	}
+	return
+}
+func main() {
+	client := &http.Client{}
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide a link.")
+		return
+	}
+	link := os.Args[1]
+	timeout := int32(5000) // timeout in milliseconds
+	rtt, err := UrlTest(client, link, timeout)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("RTT:", rtt, "ms")
+	}
+
+}
