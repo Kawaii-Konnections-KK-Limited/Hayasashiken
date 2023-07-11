64cd3ec25310dccb5f0f794b07f7433b3e83e447
parsa <hparsa1378@gmail.com>
2023-07-11 15:49:21 +0330

    added singbox and can take simple test based on config provided

---
+++ app/runbox.go
@@ -0,0 +1,113 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	box "github.com/sagernet/sing-box"
+	"github.com/sagernet/sing-box/log"
+	"github.com/sagernet/sing-box/option"
+	E "github.com/sagernet/sing/common/exceptions"
+)
+
+var disableColor bool
+
+func create(wg *sync.WaitGroup) (*box.Box, context.CancelFunc, error) {
+	var (
+		configContent []byte
+		err           error
+	)
+	if err != nil {
+		fmt.Printf("error")
+	}
+	configContent, err = os.ReadFile("config.json")
+	if err != nil {
+		fmt.Printf("file doesnt exist")
+	}
+	var options option.Options
+	err = options.UnmarshalJSON(configContent)
+
+	if err != nil {
+		return nil, nil, err
+	}
+	if disableColor {
+		if options.Log == nil {
+			options.Log = &option.LogOptions{}
+		}
+		options.Log.DisableColor = true
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	instance, err := box.New(box.Options{
+		Context: ctx,
+		Options: options,
+	})
+	if err != nil {
+		cancel()
+		return nil, nil, E.Cause(err, "create service")
+	}
+	wg.Done()
+	osSignals := make(chan os.Signal, 1)
+	signal.Notify(osSignals, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP)
+	defer func() {
+		signal.Stop(osSignals)
+		close(osSignals)
+	}()
+
+	go func() {
+		_, loaded := <-osSignals
+		if loaded {
+			cancel()
+		}
+	}()
+	err = instance.Start()
+	if err != nil {
+		cancel()
+		return nil, nil, E.Cause(err, "start service")
+	}
+	return instance, cancel, nil
+}
+func closeMonitor(ctx context.Context) {
+	time.Sleep(3 * time.Second)
+	select {
+	case <-ctx.Done():
+		return
+	default:
+	}
+	log.Fatal("sing-box did not close!")
+}
+func Run(wg *sync.WaitGroup) error {
+	osSignals := make(chan os.Signal, 1)
+	signal.Notify(osSignals, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP)
+	defer signal.Stop(osSignals)
+	for {
+		instance, cancel, err := create(wg)
+		if err != nil {
+			return err
+		}
+
+		for {
+			osSignal := <-osSignals
+			if osSignal == syscall.SIGHUP {
+
+				if err != nil {
+					log.Error(E.Cause(err, "reload service"))
+					continue
+				}
+			}
+			cancel()
+			closeCtx, closed := context.WithCancel(context.Background())
+			go closeMonitor(closeCtx)
+			instance.Close()
+			closed()
+			if osSignal != syscall.SIGHUP {
+				return nil
+			}
+			break
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,78 +1,36 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"math/rand"
-	"net"
 	"net/http"
+	"net/url"
 	"os"
-	"strings"
-	"time"
+	"sync"
+
+	"github.com/uussoop/v2ray_test/app"
 )
 
-func UrlTest(client *http.Client, link string, timeout int32) (int32, error) {
-	if client == nil {
-		return 0, fmt.Errorf("no client")
-	}
-
-	// Test handshake time
-	var time_start time.Time
-	var times = 1
-	var rtt_times = 1
-
-	// Test RTT "true delay"
-	if link2 := strings.TrimLeft(link, "true"); link != link2 {
-		link = link2
-		times = 3
-		rtt_times = 2
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Millisecond)
-	defer cancel()
-
-	req, err := http.NewRequestWithContext(ctx, "GET", link, nil)
-	req.Header.Set("User-Agent", fmt.Sprintf("curl/7.%d.%d", rand.Int()%84, rand.Int()%2))
-	if err != nil {
-		return 0, err
-	}
-
-	for i := 0; i < times; i++ {
-		if i == 1 || times == 1 {
-			time_start = time.Now()
-		}
-
-		resp, err := client.Do(req)
-		if err != nil {
-			return 0, err
-		}
-		resp.Body.Close()
-	}
-
-	return int32(time.Since(time_start).Milliseconds() / int64(rtt_times)), nil
-}
-
-func TcpPing(address string, timeout int32) (ms int32, err error) {
-	startTime := time.Now()
-	c, err := net.DialTimeout("tcp", address, time.Duration(timeout)*time.Millisecond)
-	endTime := time.Now()
-	if err == nil {
-		ms = int32(endTime.Sub(startTime).Milliseconds())
-		c.Close()
-	}
-	return
-}
 func main() {
-	client := &http.Client{}
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go app.Run(&wg)
+	wg.Wait()
+	// time.Sleep(5 * time.Second)
+	proxyUrl, err := url.Parse("http://127.0.0.1:8081")
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	client := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
 	if len(os.Args) < 2 {
 		fmt.Println("Please provide a link.")
 		return
 	}
 	link := os.Args[1]
-	timeout := int32(5000) // timeout in milliseconds
-	rtt, err := UrlTest(client, link, timeout)
-	if err != nil {
-		fmt.Println("Error:", err)
+	timeout := int32(50000) // timeout in milliseconds
+	rtt, testerr := app.UrlTest(client, link, timeout)
+	if testerr != nil {
+		fmt.Println("Error:", testerr)
 	} else {
 		fmt.Println("RTT:", rtt, "ms")
 	}
