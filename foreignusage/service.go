2d8af77845ad11d62e5b4beb51201286aeaba57c
parsa <parsa@beastbrain.org>
2023-09-02 14:55:08 +0330

    edited api service to accept tls as well if given nil it will run as usual

--- foreignusage/service.go
+++ foreignusage/service.go
@@ -3,25 +3,41 @@ package foreignusage
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/Kawaii-Konnections-KK-Limited/Hayasashiken/foreignusage/api"
 )
 
-func InitService() {
+func InitService(certFile, keyFile *string) {
 
 	port := os.Getenv("PORT")
+	addr := os.Getenv("ADDRESS")
+
 	if port == "" {
 		fmt.Println(errors.New("please set PORT environment variable"))
 		port = "8080"
 		fmt.Println("Defaulting to port ", port)
 
 	}
-
-	err := api.InitRouter().Run(":" + port)
-
-	if err != nil {
-		return
+	if addr == "" {
+		fmt.Println(errors.New("please set ADDRESS environment variable"))
+		addr = "0.0.0.0"
+		fmt.Println("Defaulting to addr ", addr)
 	}
+	if addr != "" && certFile != nil && keyFile != nil {
+		log.Println("running with tls")
+		api.InitRouter().RunTLS(fmt.Sprintf("%s:%s", addr, port), *certFile, *keyFile)
+	} else {
+		log.Println("running without tls")
+
+		api.InitRouter().Run(fmt.Sprintf("%s:%s", addr, port))
+	}
+
+	// err := api.InitRouter().Run(":" + port)
+
+	// if err != nil {
+	// 	return
+	// }
 
 }
