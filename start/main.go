618ce082e6004144eb464da3bb9855796dbb17f5
parsa <parsa@beastbrain.org>
2023-09-10 22:52:21 +0330

    some additions

--- start/main.go
+++ start/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	_ "net/http/pprof"
+
 	"github.com/Kawaii-Konnections-KK-Limited/Hayasashiken/foreignusage"
 )
 
 func main() {
 
+	go func() {
+		fmt.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
 	foreignusage.InitService(nil, nil)
 }
