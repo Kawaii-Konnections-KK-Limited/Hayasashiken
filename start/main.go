1890592bfd38e185f33e6406744dbe78e7e6103a
parsa <hparsa1378@gmail.com>
2023-09-11 19:24:26 +0330

    commenting profiling as it was a mistake to push it

--- start/main.go
+++ start/main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	_ "net/http/pprof"
+
+	// _ "net/http/pprof"
 
 	"github.com/Kawaii-Konnections-KK-Limited/Hayasashiken/foreignusage"
 )
 
 func main() {
 
-	go func() {
-		fmt.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
+	// go func() {
+	// 	fmt.Println(http.ListenAndServe("localhost:6060", nil))
+	// }()
 	foreignusage.InitService(nil, nil)
 }
